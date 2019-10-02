db1dfd7cadcc3b8cb64f3fcd5a2f7996a815a510
Jose Andres S <joseandresalazar@gmail.com>
2019-10-02 11:15:32 -0500

    Revert "Sets project structure"
    This reverts commit 1ec2bc5d26cb819161f3a4d3835293608e7502bd.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
-import "./App.css";
-import { Routes } from "./ui/Routes";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <Routes />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
