accc49f169d93cf6bd5972d02b163e3629911e3d
isabelaescalante <isaescalante123@gmail.com>
2019-10-02 14:27:48 -0500

    Antd and signup page added

---
+++ src/signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { version, Button } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+
+ReactDOM.render(
+    
+);
