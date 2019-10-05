3dddbda833bb680e0ad4c00412f37ed0a96179d2
Denisse <denissedam@gmail.com>
2019-10-04 20:15:05 -0500

    add foot and header1

--- src/ui/components/Footer.js
+++ src/ui/components/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 export default class Footer extends React.Component {
   render() {
     return (
       <div>
-        <h1>Footer</h1>
+        <Card.Footer className="text-muted">
+          <small>Politicas PickPack</small>
+          <br/>
+          <small>#PickPackContigo</small>
+          <br/>
+          <small>¿Quieres recibir Tips y Ofertas de Viaje? </small>
+        </Card.Footer>
       </div>
     );
   }
--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Logo } from "../assets/logo.png";
 
 export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Header</h1>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Inicio</Navbar.Brand>
+          <NavDropdown title="Viajes" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/2.1">Tours</NavDropdown.Item>
+            <NavDropdown.Item href="#action/2.2">Actividades</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link>Provedores</Nav.Link>
+          <Nav.Link>Blog</Nav.Link>
+        </Navbar>
       </div>
     );
   }
