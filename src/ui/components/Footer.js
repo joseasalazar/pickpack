d58bd41ad73940fb4f9a8a95c4d808384ae0b951
denisse96 <denissedam@gmail.com>
2019-10-09 13:39:04 -0500

    Revert "Denisse2 (#8)"
    This reverts commit cf877d7401afd87316e62cea0599ffb8464bf518.

--- src/ui/components/Footer.js
+++ src/ui/components/Footer.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Card } from "react-bootstrap";
 
 export class Footer extends React.Component {
   render() {
     return (
       <div>
-        <Card.Footer className="text-muted">
-          <small>¿Quieres recibir Tips y Ofertas de Viaje? </small>
-        </Card.Footer>
+        <h1>Footer</h1>
       </div>
     );
   }
