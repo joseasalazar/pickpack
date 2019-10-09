c943b882a7dd1e2577cf82cee0e1796fa1d8b4a9
Denisse <denissedam@gmail.com>
2019-10-09 02:19:34 -0500

    HomeScreen with Typist

--- src/ui/screens/HomeScreen.js
+++ src/ui/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { Card, Figure } from "react-bootstrap";
-
+import styled from "styled-components";
 import BG from "../assets/viajaresincreible.jpeg";
 import BG1 from "../assets/guysinsnow.jpg";
+import Typist from "react-typist";
+//import InputCard from "../components/InputDestination";
+
+const Title = styled(Card.Title)`
+  text-align: left;
+  color: white;
+  font-family: "Roboto", sans-serif;
+  font-size: xx-large;
+`;
+
+const subTitle = {
+  textAlign: "center",
+  color: "black",
+  fontFamily: "Roboto, sans-serif"
+};
 
 export class HomeScreen extends React.Component {
   render() {
@@ -11,19 +26,24 @@ export class HomeScreen extends React.Component {
         <Card className="text-white">
           <Card.Img src={BG} alt="guy with bicycle" />
           <Card.ImgOverlay>
-            <Card.Title>PickPack</Card.Title>
-            <Card.Text>¡Ve lejos, comienza aquí!</Card.Text>
+            <Card.Title>
+              <Title>PickPack</Title>
+            </Card.Title>
+            <Typist style={subTitle}>¡Ve lejos, comienza aquí</Typist>
           </Card.ImgOverlay>
         </Card>
+
         <Card className="text-white">
           <Card.Img src={BG1} alt="guys in the snow" />
           <Card.ImgOverlay>
-            <Card.Title>#Packer</Card.Title>
+           <Title>#Packers</Title>
             <Card.Text>
               Creemos en lugares, en personas, en experiencias. Creemos en
-              viajes, historias, vivencias. Creemos en viajeros, turistas y
-              aventureros. Pero sobre todo, creemos en ti y en la libertad que
-              mereces.
+              viajes, historias, vivencias.
+              <Card.Text>
+                Creemos en viajeros, turistas y aventureros. Pero sobre todo,
+                creemos en ti y en la libertad que mereces.
+              </Card.Text>
             </Card.Text>
           </Card.ImgOverlay>
         </Card>
