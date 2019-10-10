4b076455a0231368d9cdd30bbb0782e362b4b6b2
Carolina Galván <33789544+carolinagalvan@users.noreply.github.com>
2019-10-09 23:33:07 -0500

    Added tours screen and tour card component (#13)
    * Added Log in page
    
    * Deleted antd, added react-bootstrap
    
    * Added Providers page with html initial layout.
    
    * Review changes

--- src/ui/Routes.js
+++ src/ui/Routes.js
@@ -7,6 +7,7 @@ import { Error404Screen } from "./screens/Errors/Error404Screen";
 import { LogIn } from "./screens/LogIn";
 import { SignUp } from "./screens/SignUp";
 import { Proveedores } from "./screens/Provedores";
+import { Tours } from "./screens/Tours";
 
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -22,6 +23,8 @@ export class Routes extends React.Component {
             <Route exact path="/login" component={LogIn} />
             <Route exact path="/proveedores" component={Proveedores} />
             <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/proveedores" component={Proveedores} />
+            <Route exact path="/tours" component={Tours} />
             <Route exact path="" component={Error404Screen} />
           </Switch>
           <Footer />
