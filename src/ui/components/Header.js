7e8af7216bb1fb15326aa65cb44b5ae4bea0746c
Carolina Galván <carolina.galvanv@gmail.com>
2019-10-15 21:24:16 -0500

    Added CSS to login and providers page

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -16,27 +16,29 @@ export class Header extends React.Component {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-          <Navbar.Brand href="/">            
-          <img
+          <Navbar.Brand href="/">
+            <img
               src={Logo}
               width="35"
               height="35"
               className="d-inline-block align-top"
               alt="PickPack Logo"
             ></img>
-            </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <NavDropdown title="Viajes" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/2.1">Tours</NavDropdown.Item>
-                <NavDropdown.Item href="#action/2.2">Actividades</NavDropdown.Item>
+                <NavDropdown.Item href="#action/2.2">
+                  Actividades
+                </NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="/proveedores">Proveedores</Nav.Link>
               <Nav.Link href="/contacto">Contacto</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="/login">Log in</Nav.Link>
+              <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
--- src/ui/screens/Provedores.js
+++ src/ui/screens/Provedores.js
@@ -1,25 +1,88 @@
 import React from "react";
 import styled from "styled-components";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import Picker from "../assets/provider.jpg";
+import Clock from "../assets/clock.png";
+import Seat from "../assets/seat.png";
+import Client from "../assets/client.png";
+import Alps from "../assets/alps.jpg";
+
+import { Container, Row, Col, Button, Image, Card } from "react-bootstrap";
+
+const StyledContainer = styled.div`
+  padding-left: 80px;
+  padding-right: 80px;
+`;
 
 const StyledRow = styled(Row)`
-  padding: 70px;
+  margin-bottom: 70px;
 `;
+
+const IntroStyle = {
+  textAlign: "center"
+};
+
+const MainTitleStyle = {
+  textAlign: "center",
+  color: "white",
+  fontWeight: "300",
+  fontSize: "80px",
+  marginTop: "70px"
+};
+
+const ImageText = {
+  textAlign: "center",
+  color: "black",
+  fontWeight: "300",
+  fontSize: "53px",
+  marginTop: "20px"
+};
+
+const TitleStyle = {
+  textAlign: "center",
+  fontWeight: "300",
+  fontSize: "70px",
+  marginBottom: "20px"
+};
+
+const SubTitleStyle = {
+  textAlign: "center",
+  fontWeight: "300",
+  fontSize: "40px",
+  marginBottom: "20px"
+};
+
+const Pstyle = {
+  textAlign: "center",
+  fontSize: "18px"
+};
+
+const IconStyle = {
+  maxWidth: "80px",
+  padding: "15px"
+};
+
 export class Proveedores extends React.Component {
   render() {
     return (
       <div>
-        <Container>
+        <StyledRow>
+          <Card className="text-white">
+            <div className="overflow">
+              <Card.Img src={Picker} alt="picker" />
+            </div>
+            <Card.ImgOverlay style={IntroStyle}>
+              <Card.Title>
+                <h1 style={MainTitleStyle}>¡Bienvenido Picker!</h1>
+              </Card.Title>
+              <Button variant="dark">¿Como funciona?</Button>
+            </Card.ImgOverlay>
+          </Card>
+        </StyledRow>
+
+        <h2 style={TitleStyle}>¿Qué es un Picker?</h2>
+        <StyledContainer>
           <StyledRow>
-            <Col sm={4}>
-              <h2>¡Bienvenido Picker!</h2>
-              <Button variant="primary">¿Como funciona?</Button>
-            </Col>
-            <Col sm={8}>IMAGE GOES HERE</Col>
-          </StyledRow>
-          <StyledRow>
-            <h2>¿Qué es un Picker</h2>
-            <p>
+            <p style={Pstyle}>
               Los <b>Pickers</b> son todos aquellos <b>operadores de turismo</b>{" "}
               que necesitan ayuda para <b>optimizar</b> un poco su trabajo y
               poder <b>llenar su cupo</b> más rápido, <b>PickPack</b> les da la
@@ -28,46 +91,81 @@ export class Proveedores extends React.Component {
               <b>menor precio.</b>
             </p>
           </StyledRow>
+
+          <hr></hr>
+
           <StyledRow>
             <Col>
-              ICON GOES HERE
-              <h2>Tiempo</h2>
-              <p>
+              <Card.Img
+                src={Clock}
+                alt="time"
+                style={IconStyle}
+                className="col-4 mx-auto d-flex justify-content-center flex-wrap"
+              />
+              <h2 style={SubTitleStyle}>Tiempo</h2>
+              <p style={Pstyle}>
                 Ahorra tiempo y esfuerzo al colocar tus ofertas en la
                 plataforma.
               </p>
             </Col>
             <Col>
-              ICON GOES HERE
-              <h2>Asientos Vacíos</h2>
-              <p>
+              <Card.Img
+                src={Seat}
+                alt="seat"
+                style={IconStyle}
+                className="col-4 mx-auto d-flex justify-content-center flex-wrap"
+              />
+              <h2 style={SubTitleStyle}>Asientos Vacíos</h2>
+              <p style={Pstyle}>
                 Logra llenar tus asientos vacíos de último minuto para tener el
                 menor número de pérdidas posibles.
               </p>
             </Col>
             <Col>
-              ICON GOES HERE
-              <h2>Clientes</h2>
-              <p>
+              <Card.Img
+                src={Client}
+                alt="client"
+                style={IconStyle}
+                className="col-4 mx-auto d-flex justify-content-center flex-wrap"
+              />
+              <h2 style={SubTitleStyle}>Clientes</h2>
+              <p style={Pstyle}>
                 Logra un alcance a un mayor número de clientes potenciales por
                 medio de PickPack.
               </p>
             </Col>
           </StyledRow>
+        </StyledContainer>
+
+        <StyledRow>
+          <Card className="text-white">
+            <div className="overflow">
+              <Card.Img src={Alps} alt="travel" />
+            </div>
+            <Card.ImgOverlay style={IntroStyle}>
+              <Card.Title>
+                <h2
+                  style={ImageText}
+                  // className="col-4 mx-auto d-flex justify-content-center flex-wrap"
+                >
+                  ¡Genera ofertas y convierte tus pérdidas en ganancias!
+                </h2>
+              </Card.Title>
+            </Card.ImgOverlay>
+          </Card>
+        </StyledRow>
+
+        <h2 style={TitleStyle}> ¡Bienvenido a la familia PickPack!</h2>
+        <StyledContainer>
           <StyledRow>
-            <h2>¡Genera ofertas y convierte tus pérdidas en ganancias!</h2>
-            BACKGROUND IMAGE HERE
-          </StyledRow>
-          <StyledRow>
-            <h2>¡Bienvenido a la familia PickPack!</h2>
-            <p>
+            <p style={Pstyle}>
               ¡Muchas gracias por tu confianza! Nos encanta poder trabajar
               contigo, para más información ve al botón en la parte superior que
-              dice: ¿Cómo funciona?, ¡recuerda que esto es solo el comienzo de
-              un camino lleno de aventuras!
+              dice: ¿Cómo funciona? ¡Recuerda que esto es solo el comienzo de un
+              camino lleno de aventuras!
             </p>
           </StyledRow>
-        </Container>
+        </StyledContainer>
       </div>
     );
   }
