ff9393b46f9e20f0d924804813777fc9f86097d4
Miguel Bazán <miguelb1995@icloud.com>
2019-10-15 19:10:19 -0500

    Merge dev into master  (#17)
    * Fix Heads in index.js (#11)
    
    *  Added tours screen and tour card component (#13)
    
    * Added Log in page
    
    * Deleted antd, added react-bootstrap
    
    * Added Providers page with html initial layout.
    
    * Review changes
    
    * Added new functions for API (#14)
    
    * Added Login page
    
    * Sets project configuration for api
    
    * Delete Login screen
    
    Deleting the Login screen from this branch so it won't update from the changes in master
    
    * Added Role Manager, IsLoggedIn, LogOut, resolvers, schema and loginForm functions
    
    * Adds logic to upload image to server (#16)
    
    * Modified header (#15)
    
    * Added sign up page
    
    * Sign up added
    
    * Resolved merge conflicts
    
    * Fix Heads in index.js (#11) (#12)
    
    * Modified header and styled log in

--- src/ui/screens/LogIn.js
+++ src/ui/screens/LogIn.js
@@ -1,85 +1,22 @@
 import React from "react";
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
-import { Button, Container, Row } from "react-bootstrap";
-import * as Yup from "yup";
-import { Formik, Field, Form } from "formik";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { LoginForm } from "../components/LoginForm";
+import { LOGIN_USER } from "../../api/mutations";
 
-const LoginCard = styled.div`
-  padding: 20px;
-  width: 30%;
-  margin: auto;
-  box-sizing: border-box;
-  min-width: 300px;
-`;
+export default function LogIn() {
+  const client = useApolloClient();
+  const [login, { loading, error }] = useMutation(LOGIN_USER, {
+    onCompleted({ login }) {
+      localStorage.setItem("token", login.token);
+      localStorage.setItem("role", login.user.type);
+      client.writeData({
+        data: { isLoggedIn: true, userType: login.user.type }
+      });
+    }
+  });
 
-const ErrorSpan = styled.p`
-  color: red;
-  font-size: 15;
-  text-align: "left";
-`;
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>An error occurred</p>;
 
-const SingupSchema = Yup.object().shape({
-  email: Yup.string()
-    .required("El correo es requerido.")
-    .email("Por favor ingresa un correo electrónico válido."),
-
-  password: Yup.string()
-    .min(6, "La contraseña es mínimo de 6 caracteres.")
-    .required("La contraseña es requerida.")
-});
-
-export class LogIn extends React.Component {
-  render() {
-    return (
-      <LoginCard>
-        <Formik
-          initialValues={{
-            email: "",
-            password: ""
-          }}
-          validationSchema={SingupSchema}
-          onSubmit={(values, { resetForm, setError, setSubmitting }) => {
-            console.log(values);
-          }}
-        >
-          {({ values, errors, touched }) => (
-            <Form className="form" role="form" method="post">
-              <div className="form-group">
-                <label>Correo electrónico</label>
-                <Field
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                />
-                {touched.email && errors.email && (
-                  <ErrorSpan>{errors.email}</ErrorSpan>
-                )}
-              </div>
-              <div className="form-group">
-                <label>Contraseña</label>
-                <Field
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  name="password"
-                />
-                {touched.password && errors.password && (
-                  <ErrorSpan>{errors.password}</ErrorSpan>
-                )}
-              </div>
-              <Button type="submit" variant="primary" size="md" block>
-                Iniciar sesión
-              </Button>
-              <Container>
-                <Row className="justify-content-md-center"><NavLink to="/signup"> Crear una cuenta</NavLink></Row>
-                <Row className="justify-content-md-center"><NavLink to="/forgot-password"> He olvidado mi contraseña </NavLink></Row>
-              </Container>
-            </Form>
-          )}
-        </Formik>
-      </LoginCard>
-    );
-  }
+  return <LoginForm login={login} />;
 }
