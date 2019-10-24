a887b559ab1c2e2474e4a95e65552307cac136fa
Jose Andres S <joseandresalazar@gmail.com>
2019-10-23 22:36:50 -0500

    Sends user to home page and sets logout button on log in

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -3,7 +3,8 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../assets/logo.png";
 import styled from "styled-components";
-
+import { IsLoggedIn } from "./IsLoggedIn";
+import { Logout } from "./Logout";
 import { NavLink } from "react-router-dom";
 
 const NavLinks = styled(Nav.Link)`
@@ -24,20 +25,24 @@ export class Header extends React.Component {
               className="d-inline-block align-top"
               alt="PickPack Logo"
             ></img>
-            </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <NavDropdown title="Viajes" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/2.1">Tours</NavDropdown.Item>
-                <NavDropdown.Item href="#action/2.2">Actividades</NavDropdown.Item>
+                <NavDropdown.Item href="#action/2.2">
+                  Actividades
+                </NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="/proveedores">Proveedores</Nav.Link>
               <Nav.Link href="/contacto">Contacto</Nav.Link>
             </Nav>
-            <Nav>
-              <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
-            </Nav>
+            <IsLoggedIn true={<Logout />}>
+              <Nav>
+                <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
+              </Nav>
+            </IsLoggedIn>
           </Navbar.Collapse>
         </Navbar>
       </div>
