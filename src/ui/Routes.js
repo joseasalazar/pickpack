0a74d0c7a5f9806400005a6fb2a18f590c2f24f1
Jose Andres S <joseandresalazar@gmail.com>
2019-11-06 23:34:43 -0600

    Simulates tours deletions, sets footer to bottom page

--- src/ui/Routes.js
+++ src/ui/Routes.js
@@ -22,19 +22,29 @@ export class Routes extends React.Component {
       <BrowserRouter>
         <div>
           <Header />
-          <Switch>
-            <Route exact path="/" component={HomeScreen} />
-            <Route exact path="/login" component={LogIn} />
-            <Route exact path="/proveedores" component={Proveedores} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/proveedores" component={Proveedores} />
-            <Route exact path="/comofunciona" component={ComoFunciona} />
-            <Route exact path="/compras" component={ShoppingCart} />
-            <Route exact path="/tours" component={TourScreen} />
-            <Route exact path="/tours/info" component={TourDescriptionScreen} />
-            <Route exact path="/registertourtrial" component={Registertourtrial} />
-            <Route exact path="" component={Error404Screen} />
-          </Switch>
+          <div className="bodyDiv">
+            <Switch>
+              <Route exact path="/" component={HomeScreen} />
+              <Route exact path="/login" component={LogIn} />
+              <Route exact path="/proveedores" component={Proveedores} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/proveedores" component={Proveedores} />
+              <Route exact path="/comofunciona" component={ComoFunciona} />
+              <Route exact path="/compras" component={ShoppingCart} />
+              <Route exact path="/tours" component={TourScreen} />
+              <Route
+                exact
+                path="/tours/info"
+                component={TourDescriptionScreen}
+              />
+              <Route
+                exact
+                path="/registertourtrial"
+                component={Registertourtrial}
+              />
+              <Route exact path="" component={Error404Screen} />
+            </Switch>
+          </div>
           <Footer />
         </div>
       </BrowserRouter>
